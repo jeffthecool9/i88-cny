c62bce7094a8420b67c72b78bb77a6b6f9c7c2d6
jeffthecool9 <107611378+jeffthecool9@users.noreply.github.com>
2026-01-07 11:30:50 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
-function App() {
-  return (
-    <div style={{ padding: 40, color: "white" }}>
-      <h1 style={{ fontSize: 40, fontWeight: 900 }}>Mounted ✅</h1>
-      <p>If you see this, React is running. Problem is inside your components.</p>
-    </div>
-  );
-}
-
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
