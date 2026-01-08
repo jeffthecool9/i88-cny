68a4b60ba0fcdd35191c7a27a45e829a3abc2ccf
jeffthecool9 <107611378+jeffthecool9@users.noreply.github.com>
2026-01-08 18:05:44 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import Hero from "./components/Hero";
+import Mechanics from "./components/Mechanics";
 
-
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <main className="min-h-screen bg-[#0c0101]">
+      <Hero />
+      <Mechanics />
+    </main>
+  );
+}
