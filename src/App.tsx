c3a619425fbea3cbb9a8186e2f00d7671bf9bc81
jeffthecool9 <107611378+jeffthecool9@users.noreply.github.com>
2026-01-26 11:33:20 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 
-import Hero from "./src/components/Hero";
-import Mechanics from "./src/components/Mechanics";
-import HowToJoin from "./src/components/HowToJoin";
-import FooterCTA from "./src/components/FooterCTA";
-import MiniGame from "./src/components/MiniGame";
-import DivineFortuneBox from "./src/components/DivineFortuneBox";
-import TutorialModal from "./src/components/TutorialModal";
+import Hero from "./components/Hero";
+import Mechanics from "./components/Mechanics";
+import HowToJoin from "./components/HowToJoin";
+import FooterCTA from "./components/FooterCTA";
+import MiniGame from "./components/MiniGame";
+import DivineFortuneBox from "./components/DivineFortuneBox";
+import TutorialModal from "./components/TutorialModal";
+
 
 const App: React.FC = () => {
   const [isGameOpen, setIsGameOpen] = useState(false);
