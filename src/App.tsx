1e4e2a57146007569dc156063fc5624692e9b310
jeffthecool9 <107611378+jeffthecool9@users.noreply.github.com>
2026-01-26 14:31:51 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import Hero from "./components/Hero";
 import Mechanics from "./components/Mechanics";
 import HowToJoin from "./components/HowToJoin";
-import FooterCTA from "./components/FooterCTA";
 import MiniGame from "./components/MiniGame";
 import DivineFortuneBox from "./components/DivineFortuneBox";
 import TutorialModal from "./components/TutorialModal";
